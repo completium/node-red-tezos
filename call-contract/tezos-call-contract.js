05b4cb23f594903d11f0d1d1770b0862a247b058
Benoît Rognier <benoit.rognier@edulera.com>
2020-04-01 19:48:14 +0200

    now dyn call on param or payload

--- call-contract/tezos-call-contract.js
+++ call-contract/tezos-call-contract.js
@@ -1,6 +1,22 @@
 module.exports = function(RED) {
     'use stric';
     const { Tezos } = require('@taquito/taquito');
+    function executeFunctionByName(functionName, context /*, args */) {
+        //var args = Array.prototype.slice.call(arguments, 2);
+        var args = arguments[2];
+        var namespaces = functionName.split(".");
+        var func = namespaces.pop();
+        for(var i = 0; i < namespaces.length; i++) {
+          context = context[namespaces[i]];
+        }
+        //console.log(args);
+        return context[func].apply(context, args);
+    }
+    function hasOwnProperty(obj, prop) {
+        var proto = obj.__proto__ || obj.constructor.prototype;
+        return (prop in obj) &&
+            (!(prop in proto) || proto[prop] !== obj[prop]);
+    }
     function TezosCallContract(config) {
         RED.nodes.createNode(this,config);
         this.rpc = config.rpc;
@@ -11,9 +27,16 @@ module.exports = function(RED) {
         this.mnemonic = obj.mnemonic.join(' ');
         this.secret = obj.secret;
         this.entry = config.entry;
-        this.arg = config.arg;
+        this.args = [config.arg];
         var node = this;
         node.on('input', function(msg) {
+            // overwrite node parameter with payload data
+            if (hasOwnProperty(msg.payload,'entry')) {
+                node.entry = msg.payload.entry;
+            }
+            if (hasOwnProperty(msg.payload,'args')) {
+                node.args = msg.payload.args;
+            }
             Tezos.setProvider({ rpc: node.rpc });
             Tezos.importKey(
                 node.email,
@@ -23,33 +46,28 @@ module.exports = function(RED) {
             );
             Tezos.contract.at(node.addr)
             .then(contract => {
-                const i = 7;
-                console.log(`Incrementing storage value by ${i}...`);
-                return contract.methods.increment(i).send();
+                console.log(`Calling "${node.entry}" with arg ${node.args}...`);
+                this.status({fill:"grey",shape:"dot",text:"calling ..."});
+                return executeFunctionByName("methods."+node.entry, contract, node.args).send();
             })
             .then(op => {
                 console.log(`Waiting for ${op.hash} to be confirmed...`);
+                this.status({fill:"blue",shape:"dot",text:"waiting for confirmation ..."});
                 return op.confirmation(1).then(() => op.hash);
             })
             .then(hash => {
-                    console.log(`Operation injected: https://carthagenet.tzstats.com/${hash}`);
-                    msg.payload = { res:true, op:hash };
-                    node.send(msg);
-                })
+                console.log(`Operation injected: https://carthagenet.tzstats.com/${hash}`);
+                this.status({fill:"green",shape:"dot",text:"operation injected!"});
+                msg.payload = { res:true, op:hash };
+                node.send(msg);
+            })
             .catch(error => {
-                  println(`Error: ${JSON.stringify(error, null, 2)}`);
-                  msg.payload = { res:false };
-                  node.send(msg);
+                console.log(`Error: ${JSON.stringify(error, null, 2)}`);
+                this.status({fill:"red",shape:"ring",text:"fail"});
+                msg.payload = { res:false };
+                node.send(msg);
             });
         });
-        /* Tezos.tz
-                .getBalance(node.addr)
-                .then(balance => {
-                    msg.payload = balance.toNumber() / 1000000;
-                    node.send(msg);
-                })
-                .catch(error => console.log(JSON.stringify(error)));
-        }); */
     }
     RED.nodes.registerType("tezos-call-contract",TezosCallContract);
 }
