36f36ae4e394ef5b7cb6d6ce6aeaed3f67ac93cc
Benoît Rognier <benoit.rognier@edulera.com>
2020-04-01 07:41:25 +0200

    adds faucet param

--- call-tezos-contract.js
+++ call-tezos-contract.js
@@ -5,8 +5,14 @@ module.exports = function(RED) {
         RED.nodes.createNode(this,config);
         this.rpc = config.rpc;
         this.addr = config.addr;
+        var obj = JSON.parse(config.faucet);
+        this.email = obj.email;
+        this.password = obj.password;
+        this.mnemonic = obj.mnemonic.join(' ');
+        this.secret = obj.secret;
         var node = this;
         node.on('input', function(msg) {
+            console.log(node.mnemonic);
             Tezos.setProvider({ rpc: node.rpc });
             Tezos.tz
                 .getBalance(node.addr)
