aa0083457b550dece60ece7946f2fddf4afa2fd7
Benoît Rognier <benoit.rognier@edulera.com>
2020-04-17 18:19:59 +0200

    adds transfer node

---
+++ transfer/tezos-transfer.js
@@ -0,0 +1,67 @@
+module.exports = function(RED) {
+    'use stric';
+    const { Tezos } = require('@taquito/taquito');
+    var objectConstructor = ({}).constructor;
+    function hasOwnProperty(obj, prop) {
+        var proto = obj.__proto__ || obj.constructor.prototype;
+        return (obj.constructor === objectConstructor) && (prop in obj) &&
+            (!(prop in proto) || proto[prop] !== obj[prop]);
+    }
+    function TezosTransfer(config) {
+        RED.nodes.createNode(this,config);
+        this.rpc = config.rpc;
+        try {
+            var obj = JSON.parse(config.faucet);
+            this.email = obj.email;
+            this.password = obj.password;
+            this.mnemonic = obj.mnemonic.join(' ');
+            this.secret = obj.secret;
+        } catch (e) { }
+        this.destination = config.destination;
+        this.amount = config.amount;
+        //this.vars = config.vars;
+        var node = this;
+        var withInit = false;
+        node.on('input', function(msg) {
+            // overwrite node parameter with payload data
+            if (hasOwnProperty(msg.payload,'faucet')) {
+                try {
+                    var obj = JSON.parse(msg.payload.faucet);
+                    node.email = obj.email;
+                    node.password = obj.password;
+                    node.mnemonic = obj.mnemonic.join(' ');
+                    node.secret = obj.secret;
+                } catch (e) { }
+            }
+            if (hasOwnProperty(msg.payload,'destination')) {
+                node.destination = msg.payload.destination;
+            }
+            if (hasOwnProperty(msg.payload,'amount')) {
+                node.amount = msg.payload.amount;
+            }
+            Tezos.setProvider({ rpc: node.rpc });
+            Tezos.importKey(
+                node.email,
+                node.password,
+                node.mnemonic,
+                node.secret
+            );
+            this.status({fill:"grey",shape:"dot",text:"transfering ..."});
+            console.log("transfer ...");
+            Tezos.contract.transfer({ to: node.desitnation, amount: node.amount })
+            .then(op => {
+                console.log(`Waiting for ${op.hash} to be confirmed...`);
+                this.status({fill:"grey",shape:"dot",text:"confirming ..."});
+                return op.confirmation(1).then(() => op.hash);
+            })
+            .then(hash =>
+                this.status({fill:"green",shape:"dot",text:"success!"})
+            )
+            .catch(error => {
+                console.log(`Error: ${JSON.stringify(error, null, 2)}`);
+                this.status({fill:"red",shape:"dot",text:"fail!"})
+            });
+        });
+    }
+    RED.nodes.registerType("tezos-transfer",TezosTransfer);
+}
